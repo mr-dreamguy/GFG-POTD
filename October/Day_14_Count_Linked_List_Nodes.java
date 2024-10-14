aa1fdd8f8366ce274d8b4d2f5adf08d85e53f664
Baji Saidavali Shaik <109856074+mr-dreamguy@users.noreply.github.com>
2024-10-14 12:11:23 +0530

    Day 14 : 14-10-2024

---
+++ October/Day_14_Count_Linked_List_Nodes.java
@@ -0,0 +1,12 @@
+class Solution {
+    // Function to count nodes of a linked list.
+    public int getCount(Node head) {
+        int count = 0;
+        while (head != null) {
+            head = head.next;
+            count++;
+        }
+        
+        return count;
+    }
+}
