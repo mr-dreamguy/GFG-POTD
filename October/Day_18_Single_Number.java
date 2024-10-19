39e3da75b6abaca7c24644e4b0f0d29c4bc5569a
Baji Saidavali Shaik <109856074+mr-dreamguy@users.noreply.github.com>
2024-10-19 19:15:26 +0530

    Day 19 : 19-10-2024

---
+++ October/Day_18_Single_Number.java
@@ -0,0 +1,11 @@
+class Solution {
+    int getSingle(int arr[]) {
+        int result = 0;
+        
+        for (int num : arr) {
+            result ^= num;
+        }
+        
+        return result;
+    }
+}
