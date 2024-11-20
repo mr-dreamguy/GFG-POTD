b94a7886005e9c395af88261156dbaffc18d3ec7
Baji Saidavali Shaik <109856074+mr-dreamguy@users.noreply.github.com>
2024-11-20 12:29:18 +0530

    Day 20 : 20-11-2024

---
+++ November/Day_20_Majority_Element_II.java
@@ -0,0 +1,36 @@
+class Solution {
+    // Function to find the majority elements in the array
+    public List<Integer> findMajority(int[] nums) {
+        List<Integer>res=new ArrayList<>();
+        int cand1=0,cand2=0,count1=0,count2=0;
+        
+        for(int num : nums) {
+            if (num == cand1) count1++;
+            else if (num == cand2) count2++;
+            else if (count1 == 0) {
+                cand1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                cand2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+      
+        count1=0; count2=0;
+        for (int num : nums) {
+            if (num == cand1) count1++;
+            else if (num == cand2) count2++;
+        }
+
+        int n = nums.length;
+        if (count1 > n/3) res.add(cand1);
+        if (count2 > n/3) res.add(cand2);
+        
+        Collections.sort(res);
+
+        return res;
+    }
+}
