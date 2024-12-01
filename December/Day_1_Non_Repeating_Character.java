5b61aa4c33e69bea7e7988f9d4c2d2f48af2afc7
Baji Saidavali Shaik <109856074+mr-dreamguy@users.noreply.github.com>
2024-12-01 11:23:47 +0530

    Day 1 : 01-12-2024

---
+++ December/Day_1_Non_Repeating_Character.java
@@ -0,0 +1,13 @@
+class Solution {
+    // Function to find the first non-repeating character in a string.
+    static char nonRepeatingChar(String s) {
+        int[] freq = new int[26];
+        
+        for (int i = 0; i < s.length(); i++) freq[s.charAt(i)-'a']++;
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i)-'a'] == 1) return s.charAt(i);
+        }
+        
+        return '$';
+    }
+}
