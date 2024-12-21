4502ae668dfde3018c2dc7bc1ece07424010a773
Baji Saidavali Shaik <109856074+mr-dreamguy@users.noreply.github.com>
2024-12-21 23:39:58 +0530

    Day 21 : 21-12-2024

---
+++ December/Day_21_Rotate_by_90_degree.java
@@ -0,0 +1,25 @@
+class Solution
+{
+    //Function to rotate matrix anticlockwise by 90 degrees.
+    static void rotateby90(int arr[][]) { 
+        int len=arr.length;
+        for(int i=0;i<len;i++){
+           int st=0,lst=len-1;
+           while(lst>st){
+               int temp=arr[i][st];
+               arr[i][st]=arr[i][lst];
+               arr[i][lst]=temp;
+               st++;
+               lst--;
+           }
+        }
+
+        for(int i=0;i<len;i++){
+            for(int j=0;j<=i;j++){
+                int temp=arr[i][j];
+                arr[i][j]=arr[j][i];
+                arr[j][i]=temp;
+            }
+        }
+    }
+}
