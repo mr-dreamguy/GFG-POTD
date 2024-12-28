ba13f858cfaaae3d8eb053476c7572d48d49adc6
Baji Saidavali Shaik <109856074+mr-dreamguy@users.noreply.github.com>
2024-12-28 23:08:06 +0530

    Day 28 : 28-12-2024

---
+++ December/Day_28_Find_all_Triplets_with_Zero_Sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<Integer>> findTriplets(int[] arr) {
+        List<List<Integer>> list = new ArrayList<>();
+        
+        for (int i = 0; i < arr.length-2; i++) {
+            for (int j = i+1; j < arr.length-1; j++) {
+                for (int k = j+1; k < arr.length; k++) {
+                    if (arr[i]+arr[j]+arr[k] == 0) {
+                        list.add(Arrays.asList(i, j, k));
+                    }
+                }
+            }
+        }
+        
+        return list;
+    }
+}
